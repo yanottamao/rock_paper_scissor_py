ae5268c63af94f2c1bcb1f4a62d28c59ce1495a6
yanottamao <oktatama4@gmail.com>
2021-04-01 17:45:24 +0700

    Edit program to include function

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 import random
 
-# print("Hello Rock Paper Scissor")
-# comp_choices = random.randint(1, 9)
-# if comp_choices == 1 or comp_choices == 6 or comp_choices == 7:
-#     print("Rock")
-# elif comp_choices == 2 or comp_choices == 4 or comp_choices == 9:
-#     print("Paper")
-# elif comp_choices == 3 or comp_choices == 5 or comp_choices == 8:
-#     print("Scissor")
-# print(comp_choices)
 
-print("Hello Rock Paper Scissor")
-comp_choices = random.randint(1, 3)
-if comp_choices == 1:
-    print("Rock")
-elif comp_choices == 2:
-    print("Paper")
-elif comp_choices == 3:
+def choices():
+    comp_choices = random.randint(1, 3)
+    if comp_choices == 1:
+        print("Rock")
+    elif comp_choices == 2:
+        print("Paper")
+    elif comp_choices == 3:
     print("Scissor")
-print(comp_choices)
+    print(comp_choices)
+
 
 print("1. Rock")
 print("2. Paper")
@@ -26,8 +18,9 @@ print("3. Scissor")
 plyr_choices = int(input("Please enter 1/2/3: "))
 print(plyr_choices)
 
-# if plyr_choices == 1 or plyr_choices == 2 or plyr_choices == 3:
-#     print("Please enter valid choice")
+
+def win_condition():
+
 
 if comp_choices == 1 and plyr_choices == 1:
     print("Draw")
@@ -49,3 +42,9 @@ elif comp_choices == 3 and plyr_choices == 2:
     print("Computer Wins")
 elif comp_choices == 3 and plyr_choices == 3:
     print("Draw")
+
+
+def main():
+
+
+print("Hello Rock Paper Scissor")

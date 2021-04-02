1d3ae280208d05b8b1dab4c545e8d864cc3b9878
yanottamao <oktatama4@gmail.com>
2021-04-02 11:51:50 +0700

    Formatting code

--- app.py
+++ app.py
@@ -11,40 +11,39 @@ def choices():
     print("Scissor")
     print(comp_choices)
 
-
-print("1. Rock")
-print("2. Paper")
-print("3. Scissor")
-plyr_choices = int(input("Please enter 1/2/3: "))
-print(plyr_choices)
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissor")
+    plyr_choices = int(input("Please enter 1/2/3: "))
+    print(plyr_choices)
 
 
 def win_condition():
+    if comp_choices == 1 and plyr_choices == 1:
+        print("Draw")
+    elif comp_choices == 1 and plyr_choices == 2:
+        print("Player Wins")
+    elif comp_choices == 1 and plyr_choices == 3:
+        print("Computer Wins")
 
+    elif comp_choices == 2 and plyr_choices == 1:
+        print("Computer Wins")
+    elif comp_choices == 2 and plyr_choices == 2:
+        print("Draw")
+    elif comp_choices == 2 and plyr_choices == 3:
+        print("Player Wins")
 
-if comp_choices == 1 and plyr_choices == 1:
-    print("Draw")
-elif comp_choices == 1 and plyr_choices == 2:
-    print("Player Wins")
-elif comp_choices == 1 and plyr_choices == 3:
-    print("Computer Wins")
-
-elif comp_choices == 2 and plyr_choices == 1:
-    print("Computer Wins")
-elif comp_choices == 2 and plyr_choices == 2:
-    print("Draw")
-elif comp_choices == 2 and plyr_choices == 3:
-    print("Player Wins")
-
-elif comp_choices == 3 and plyr_choices == 1:
-    print("Player Wins")
-elif comp_choices == 3 and plyr_choices == 2:
-    print("Computer Wins")
-elif comp_choices == 3 and plyr_choices == 3:
-    print("Draw")
+    elif comp_choices == 3 and plyr_choices == 1:
+        print("Player Wins")
+    elif comp_choices == 3 and plyr_choices == 2:
+        print("Computer Wins")
+    elif comp_choices == 3 and plyr_choices == 3:
+        print("Draw")
 
 
 def main():
+    print("Hello Rock Paper Scissor")
 
 
-print("Hello Rock Paper Scissor")
+if __name__ == "__main__":
+    main()

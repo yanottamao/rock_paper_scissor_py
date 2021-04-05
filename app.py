9d70c3eb28b418971b988a4e01c74c046345cd32
yanottamao <oktatama4@gmail.com>
2021-04-05 21:27:40 +0700

    Edit display output

--- app.py
+++ app.py
@@ -19,30 +19,49 @@ def choices():
     print("2. Paper")
     print("3. Scissor")
     plyr_choices = int(input("Please enter 1/2/3: "))
-    print(plyr_choices)
+    # helper
+    # print(plyr_choices)
     return comp_choices, plyr_choices
 
 
 def win_condition():
     if comp_choices == 1 and plyr_choices == 1:
+        print("Computer: Rock")
+        print("Player: Rock")
         print("Result: Draw")
     elif comp_choices == 1 and plyr_choices == 2:
+        print("Computer: Rock")
+        print("Player: Paper")
         print("Result: Player Wins")
     elif comp_choices == 1 and plyr_choices == 3:
+        print("Computer: Rock")
+        print("Player: Scissor")
         print("Result: Computer Wins")
 
     elif comp_choices == 2 and plyr_choices == 1:
+        print("Computer: Paper")
+        print("Player: Rock")
         print("Result: Computer Wins")
     elif comp_choices == 2 and plyr_choices == 2:
+        print("Computer: Paper")
+        print("Player: Paper")
         print("Result: Draw")
     elif comp_choices == 2 and plyr_choices == 3:
+        print("Computer: Paper")
+        print("Player: Scissor")
         print("Result: Player Wins")
 
     elif comp_choices == 3 and plyr_choices == 1:
+        print("Computer: Scissor")
+        print("Player: Rock")
         print("Result: Player Wins")
     elif comp_choices == 3 and plyr_choices == 2:
+        print("Computer: Scissor")
+        print("Player: Paper")
         print("Result: Computer Wins")
     elif comp_choices == 3 and plyr_choices == 3:
+        print("Computer: Scissor")
+        print("Player: Scissor")
         print("Result: Draw")
 
 
